e79bfb476f86fd003904489b5968b7e5e5ffc13a
Marlon Araujo <marlonaraujo2709@gmail.com>
2024-04-20 18:16:50 -0300

    UPDATE: install a specific kubernetes version from env

--- scripts/common/add_repos.sh
+++ scripts/common/add_repos.sh
@@ -1,8 +1,9 @@
 function add_repos() {
+  SHORT_VERSION=${K8S_VERSION%.*$}
   sudo apt-get install -y apt-transport-https ca-certificates curl gpg
   sudo mkdir -p -m 755 /etc/apt/keyrings
-  curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+  curl -fsSL https://pkgs.k8s.io/core:/stable:/v$SHORT_VERSION/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+  echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v'$SHORT_VERSION'/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 }
 
 
--- scripts/common/download_k8s_binaries.sh
+++ scripts/common/download_k8s_binaries.sh
@@ -1,3 +1,3 @@
 function download_k8s_binaries() {
-  sudo apt-get install -y kubelet kubeadm kubectl
+  sudo apt-get install -y kubelet=$K8S_VERSION kubeadm=$K8S_VERSION kubectl=$K8S_VERSION
 }
